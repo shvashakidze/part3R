71623f29193aa078cf2bb5063c45febdf2acefb7
shvashakidze <shvashakidze@users.noreply.github.com>
2021-05-29 06:36:28 -0700

    morgan

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+
 
 app.use(express.json())
-
+app.use(morgan('tiny'))
 let notes = [ 
     {
       id: 1,
@@ -41,6 +43,8 @@ let notes = [
     
       response.status(204).end()
     }) 
+
+    
 
   app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
