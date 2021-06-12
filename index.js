9b0778aa03e2520595961bd8cd87a256106bda98
shvashakidze <shvashakidze@users.noreply.github.com>
2021-06-12 16:50:02 -0700

    1

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ let notes = [
     response.json(note)
   })
 
-const PORT = process.env.PORT || 3002
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
